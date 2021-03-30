e681baf073c1a3c4db3b2a89cca48eb528086826
v801 <32247825+v801@users.noreply.github.com>
2021-03-30 03:15:22 -0700

    init

---
+++ CrushAntlers/CrushAntlers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BepInEx;
+using JotunnLib.Managers;
+using JotunnLib.Entities;
+
+namespace CrushAntlers
+{
+    [BepInPlugin("com.bepinex.plugins.crushantlers", "CrushAntlers", "0.1.0")]
+    [BepInDependency("com.bepinex.plugins.jotunnlib")]
+    public class CrushAntlers : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            ObjectManager.Instance.ObjectRegister += registerObjects;
+        }
+
+        private void registerObjects(object sender, EventArgs e)
+        {
+            ObjectManager.Instance.RegisterRecipe(new RecipeConfig()
+            {
+                Name = "Recipe_CrushAntlers",
+                Item = "BoneFragments",
+                Amount = 10,
+                CraftingStation = "piece_workbench",
+
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig()
+                    {
+                        Item = "HardAntler",
+                        Amount = 1
+                    }
+                }
+            });
+        }
+    }
+}
